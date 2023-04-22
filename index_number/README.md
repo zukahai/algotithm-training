1025f208556053951d2ac470b0ce615434daadba
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2023-04-22 10:43:57 +0700

    add problem index_number

---
+++ index_number/README.md
@@ -0,0 +1,24 @@
+<div align="center">
+
+# Index Number
+
+</div>
+
+Cho một mảng số nguyên gồm N phân tử đã được sắp xếp không giảm.<br>
+Kiểm tra số nguyên X có tồn tại trong dãy không, nếu có in ra YES, ngược lại in ra NO<br>
+<br>
+Input:<br>
+    Dòng đầu tiên gồm số nguyên dương N (1 <= N <= 10^5)<br>
+    Dòng thứ 2 gồm N phần tử cách nhau bởi một dấu cách (|a[i]| <= 10^9)<br>
+    Dòng thứ 3 gồm số nguyên X (|X| <= 10^9)<br>
+Output:<br>
+    Một dòng duy nhất là kết quả cần tìm.<br>
+<br>
+
+Test case mẫu:<br>
+
+### Testcase 1:
+|Input| Output|
+|-----|:-----:|
+|5<br>1 2 2 3 4<br>2| YES|
+
---
+++ index_number/random_test_case/random_test_case.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string toString(int n)
+{
+    string ans = "";
+    while (n > 0)
+    {
+        ans = char(n % 10 + '0') + ans;
+        n /= 10;
+    }
+    if (ans.length() == 1)
+    {
+        ans = "0" + ans;
+    }
+    return ans;
+}
+
+string solve(vector<int> a, int x)
+{
+    int l = 0;
+    int r = a.size() - 1;
+    while (l <= r)
+    {
+        int m = (l + r) / 2;
+        if (a[m] == x)
+        {
+            return "YES";
+        }
+        else if (a[m] < x)
+        {
+            l = m + 1;
+        }
+        else
+        {
+            r = m - 1;
+        }
+    }
+    return "NO";
+}
+vector<int> a;
+int main()
+{
+    ofstream finput, foutput;
+
+    srand(time(NULL));
+
+    long long n, minN = 2;
+    int x;
+    for (int TC = 1; TC <= 10; TC++)
+    {
+        finput.open("../testcase/tc" + toString(TC) + "/input.txt");
+        foutput.open("../testcase/tc" + toString(TC) + "/output.txt");
+        cout << toString(TC) << endl;
+
+        n = (rand() % 10 + 1);
+        while (n < minN)
+        {
+            n = n * (rand() % 10 + 1);
+        }
+
+        finput << n << endl;
+        a.resize(n);
+        a[0] = (rand() * rand() - rand() * rand()) % minN + 1;
+        for (int i = 1; i < n; i++)
+        {
+            a[i] = a[i - 1] + rand() % 10;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            finput << a[i] << " ";
+        }
+        x = a[rand() * rand() % n] + (rand()) % 2;
+        finput << endl
+               << x;
+
+        finput.close();
+
+        foutput << solve(a, x);
+        foutput.close();
+        minN *= 10;
+        if (minN > 100000)
+        {
+            minN = 100000;
+        }
+    }
+}
